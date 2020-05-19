bb7aa8d749a242adda413c388c2cb0cc49525c23
Marco <rubolini.marco@gmail.com>
2020-05-19 18:05:25 +0200

    Esercizio

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+$('.container .griglia').on('click', '.quadrato', function(){
+    var prova = $(this);
+    $.ajax({
+        'url': 'https://flynn.boolean.careers/exercises/api/random/int',
+        'method': 'GET',
+        'success':function(data) {
+            console.log(data);
+            var numero = data.response;
+            console.log('Numero casuale ricevuto dalle API: ' + numero);
+            coloraQuadrato(numero, prova);
+
+        },
+        'error': function() {
+            alert('si è verificato un errore!')
+        }
+    })
+})
+
+function coloraQuadrato(number, quadrato) {
+    if (number <= 5) {
+        $(quadrato).addClass('yellow');
+    } else {
+        $(quadrato).addClass('green');
+    }
+}
+
+});
